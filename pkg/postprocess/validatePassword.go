ba42b884d7b77f911d270b9e7e3f291f239baa7b
Grinish Nepal <grinish.nepal@aexp.com>
2021-06-29 17:09:59 -0700

    refactor: reuse SkipSameKeyValue function and add to postprocess for category key #patch

--- pkg/postprocess/validatePassword.go
+++ pkg/postprocess/validatePassword.go
@@ -17,7 +17,6 @@
 package postprocess
 
 import (
-	"github.com/americanexpress/earlybird/pkg/utils"
 	"regexp"
 	"strconv"
 	"strings"
@@ -89,30 +88,6 @@ func PasswordFalse(password string) (confidence int, ignore bool) {
 	}
 
 	return confidence, false
-}
-
-// SkipSameKeyValuePassword reads the matched value from hit object to compare if the key and value are same for passwords or secrets.
-// UseCase: In config/properties files where key/value pair are used to inject passwords.
-func SkipSameKeyValuePassword(matchValue string, lineValue string) (result bool) {
-	findingsArray := [2]string{matchValue, lineValue}
-	for _, password := range findingsArray {
-		// Separate password from full value as key/value pair
-		splitFindings := splitPswdPattern.Split(password, -1)
-		// Check if length = 2 for true key/value pair.
-		if len(splitFindings) == 2 {
-			// Trim the spaces and remove the Quotes(single or double) if they exist and lower case it for equality check.
-			trimKey := strings.ToLower(strings.Trim(strings.TrimSpace(splitFindings[0]), "\"'"))
-			trimValue := strings.ToLower(strings.Trim(strings.TrimSpace(splitFindings[1]), "\"'"))
-
-			// Check if key and value are equal. Only validates the alphanumeric part.
-			// Might cause us to skip some real findings but has a very low possibility.
-			if strings.EqualFold(utils.GetAlphaNumericValues(trimKey), utils.GetAlphaNumericValues(trimValue)) {
-				// return early if its same key/value
-				return true
-			}
-		}
-	}
-	return false
 }
 
 // SkipPasswordWithUnicode returns true if the password value contains a non ASCII character.
