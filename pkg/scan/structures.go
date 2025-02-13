f12afaa29d8e002f61241469869f087e8ead8e3a
umaniy <utsav.maniyar1@aexp.com>
2025-02-13 07:10:33 -0700

    fix(jks-http): updated code to check for pk in jks via http

--- pkg/scan/structures.go
+++ pkg/scan/structures.go
@@ -20,13 +20,13 @@ import (
 	"regexp"
 )
 
-//Rules is the exported definition of the Rules structure for Earlybird
+// Rules is the exported definition of the Rules structure for Earlybird
 type Rules struct {
 	Rules      []Rule `json:"rules"`
 	Searcharea string `json:"Searcharea"`
 }
 
-//Rule Each module config is a set of rules
+// Rule Each module config is a set of rules
 type Rule struct {
 	Code, Severity, Confidence, SolutionID            int
 	Pattern, Caption, Category, Solution, Postprocess string
@@ -36,7 +36,7 @@ type Rule struct {
 	Example                                           string
 }
 
-//Hit is a match in a file against a specific rule
+// Hit is a match in a file against a specific rule
 type Hit struct {
 	Code         int      `json:"code"`
 	Filename     string   `json:"filename"`
@@ -60,6 +60,7 @@ type File struct {
 	Name  string
 	Path  string
 	Lines []Line
+	Raw   []byte
 }
 
 // Line in a file to scan
@@ -84,7 +85,7 @@ type Report struct {
 	Duration      string   `json:"duration"`
 }
 
-//WorkJob As we add jobs to the pool, they need to contain the line being scanned and the file content (in Lines)
+// WorkJob As we add jobs to the pool, they need to contain the line being scanned and the file content (in Lines)
 type WorkJob struct {
 	WorkLine  Line
 	FileLines []Line
@@ -95,7 +96,7 @@ type FalsePositives struct {
 	FalsePositives []FalsePositive `json:"rules"`
 }
 
-//FalsePositive is a rule to match false positives post process
+// FalsePositive is a rule to match false positives post process
 type FalsePositive struct {
 	Codes           []int
 	Pattern         string
@@ -109,13 +110,13 @@ type Solutions struct {
 	Solutions []Solution `json:"solutions"`
 }
 
-//Solution display text for a solution
+// Solution display text for a solution
 type Solution struct {
 	ID   int    `json:"id"`
 	Text string `json:"text"`
 }
 
-//LabelConfig Rule for applying labels to hits based on context
+// LabelConfig Rule for applying labels to hits based on context
 type LabelConfig struct {
 	Label     string   `json:"label"`
 	Keys      []string `json:"keys"`
@@ -124,7 +125,7 @@ type LabelConfig struct {
 	Codes     []int    `json:"codes"`
 }
 
-//LabelConfigs Rules for applying labels to hits based on context
+// LabelConfigs Rules for applying labels to hits based on context
 type LabelConfigs struct {
 	Labels []LabelConfig `json:"Labels"`
 }
