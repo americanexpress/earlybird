f01cc463099876bd6e7dec86d71de8d4ef39b3f0
Afham Fardeen <afham.fardeen@aexp.com>
2025-09-03 17:46:54 +0530

    fix: adding testcases

--- pkg/postprocess/basicAuthHeader_test.go
+++ pkg/postprocess/basicAuthHeader_test.go
@@ -21,6 +21,21 @@ func TestIsBasicAuthHeader(t *testing.T) {
 			"Authorization: Basic acdefjhikl",
 			false,
 		},
+		{
+			"Valid Basic Auth Header",
+			"Basic dXNlbmFtZTpwYXNzd29yZA==",
+			true,
+		},
+		{
+			"Empty Basic Auth Header",
+			"Authorization: ",
+			false,
+		},
+		{
+			"Missing Basic Prefix",
+			"dXNlbmFtZTpwYXNzd29yZA==",
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
